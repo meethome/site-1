386105e7eecd1905a4ee08f8756fe923165329e7
akhilesh <akhileshjyotishi1729@gmail.com>
2023-09-27 17:14:41 +0530

    navbar is made responsive by adding a new file named logo-section-2.js

---
+++ pages/trial.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LogoSection from "../components/logo-section";
+
+const Trial = () => {
+  return (
+    <div>
+      {/* <LogoSection /> */}
+    </div>
+  );
+};
+
+export default Trial;
